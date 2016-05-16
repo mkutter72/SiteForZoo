f066f3927f4979ea9fe77ca5ef2a1c567ca59674
MikeKutter <mkutter@yahoo.com>
2016-05-16 14:18:04 -0400

    Update

--- postcard.js
+++ postcard.js
@@ -4,15 +4,16 @@ $(document).ready(function () {
 
 
 
- var   imgURL = "https://kutter-001.s3.amazonaws.com/2016-04-11/ced211621f32382d33096dde5f0644ac.png";
+ var   imgURL = "https://s3.amazonaws.com/detroitzoo.postcards/dz_01.png";
 
    $('#postCardImage').attr("src", imgURL);
 
 
   $('#facebookUpload').on('click',function (e){
     e.preventDefault();
-       window.location.href = "http://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fkutter-001.s3.amazonaws.com%2F2016-04-11%2Fced211621f32382d33096dde5f0644ac.png"
+       var fbURL = "http://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(imgURL);
 
+       window.location.href = fbURL;
     $('#facebookUpload').blur();
 
   });
